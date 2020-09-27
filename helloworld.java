02d2488b52947c514325832f80256bb4eb65b999
Zeebo0 <1363667093@qq.com>
2020-09-27 14:45:05 +0800

    hello world

---
+++ helloworld.java
@@ -0,0 +1,8 @@
+public class helloworld{
+	public static void main(Stirng[] args) {
+		System.out.println("1!");
+		System.out.println("hello world!");
+		System.out.println("2£¡");
+		System.out.println("hello world!");
+	}
+}
